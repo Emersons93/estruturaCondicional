80ca8c7dc4cc816d1199883c2ac23d533b7bf658
Emerson Soares da Silva <esds.emerson1993@gmail.com>
2024-05-30 11:26:56 -0300

    gerar nota final de aluno aprovado ou reprovado.

---
+++ src/mediaFinal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class mediaFinal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+
+        double notaFinal = nota1 + nota2;
+        System.out.printf("NOTA FINAL: = %.1f%n", notaFinal);
+
+        if (notaFinal < 60.0) {
+            System.out.println("NÃO PASSOU JUMENTO");
+        }
+        else if (notaFinal > 60.0) {
+            System.out.println("DEU SORTE PASSOU RASPANDO");
+        }
+
+        sc.close();
+    }
+}
